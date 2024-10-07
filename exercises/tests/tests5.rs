c08215495d543b660eeec86c4b309a2762f93bc7
olddove-laoge <lyh102026@126.com>
2024-10-07 09:21:52 +0800

    update

--- exercises/tests/tests5.rs
+++ exercises/tests/tests5.rs
@@ -22,7 +22,6 @@
 // Execute `rustlings hint tests5` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 /// # Safety
 ///
@@ -31,8 +30,20 @@ unsafe fn modify_by_address(address: usize) {
     // TODO: Fill your safety notice of the code block below to match your
     // code's behavior and the contract of this function. You may use the
     // comment of the test below as your format reference.
+    //# Safety: The `address` must contain a mutable reference to a valid `u32` value.
+    // The `address` is guaranteed to be valid and contains a unique reference to a `u32` local variable.
+    // The `unsafe` block ensures that the reference is valid and unique, and the `todo!` macro
+    // is used to indicate the remaining work to be done.
     unsafe {
-        todo!("Your code goes here")
+        //todo!("Your code goes here")
+        // # Safety: The `address` must contain a mutable reference to a valid `u32` value.
+        // The `address` is guaranteed to be valid and contains a unique reference to a `u32` local variable.
+        // The `unsafe` block ensures that the reference is valid and unique, and the `todo!` macro
+        // is used to indicate the remaining work to be done.
+        let ptr = address as *mut u32;
+        if !ptr.is_null() {
+            *ptr = 0xAABBCCDD;  
+            }
     }
 }
 
